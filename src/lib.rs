77afed48a268d8b90a52edb4b33403f78b7862d9
Koen Bekx <klbekx@gmail.com>
2023-09-09 20:28:55 +0300

    Added a get_next_path method to Node<C>.

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,28 @@ pub struct Node<C> {
     children: Vec<Node<C>>,
 }
 
+#[derive(PartialEq)]
+#[derive(Debug)]
 pub enum PathResult {
-    Ok(Vec<usize>),
+    Found(Vec<usize>),
     RequestedPathNotAvailable,
     InputPathNotFound,
+    ProcessError,
+}
+impl PathResult {
+    pub fn is_found(&self) -> bool {
+        match self {
+            PathResult::Found(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn unwrap(self) -> Vec<usize> {
+        match self {
+            PathResult::Found(path) => path,
+            _ => panic!("The unwrap() method musn't be called on a PathResult that's not an Found variant. Check using the is_found() method first."),
+        }
+    }
 }
 
 impl<C> Node<C> {
@@ -21,15 +39,42 @@ impl<C> Node<C> {
         vec![]
     }
 
-    /*
     pub fn get_next_path(&self, path: &Vec<usize>) -> PathResult {
         let mut return_path = path.clone();
 
-        match self.borrow_node_by_path {
-            
+        match self.borrow_node_by_path(&return_path) {
+            None => PathResult::InputPathNotFound,
+            Some(nd) => {
+                if nd.children.len() > 0 {
+                    return_path.push(0);
+                    PathResult::Found(return_path)
+                } else {
+                    // Repeat this logic in a loop in order to find the next sibling
+                    // of the path node or of a parent that has one.
+
+                    let mut next_node_index: usize;
+
+                    loop {
+                        if return_path.len() == 0 {
+                            break PathResult::RequestedPathNotAvailable;
+                        }
+
+                        next_node_index = return_path.pop().unwrap() + 1;
+
+                        match self.borrow_node_by_path(&return_path) {
+                            None => break PathResult::ProcessError,
+                            Some(parent) => {
+                                if next_node_index < parent.children.len() {
+                                    return_path.push(next_node_index);
+                                    break PathResult::Found(return_path);
+                                }
+                            },
+                        }
+                    }
+                }
+            },
         }
     }
-    */
 
     pub fn add_cargo_under_path(&mut self, path: &Vec<usize>, cargo: C) -> Result<Vec<usize>, C> {
         let mut return_path = path.clone();
@@ -451,5 +496,97 @@ mod tests {
         result = n.add_cargo_before_path(&vec![2, 1], 5);
         assert!(result.is_err());
         assert_eq!(5, result.unwrap_err());
+    }
+
+    #[test]
+    fn node_get_next_path_on_lone_root() {
+        let n = Node::new(0u8);
+        assert_eq!(PathResult::RequestedPathNotAvailable, n.get_next_path(&vec![]));
+    }
+
+    #[test]
+    fn node_get_next_path_input_not_found() {
+        let n = Node::new(0u8);
+        assert_eq!(PathResult::InputPathNotFound, n.get_next_path(&vec![5]));
+    }
+
+    #[test]
+    fn node_get_next_path_from_last() {
+        let mut n = Node::new(0u8);
+        n.add_cargo_under_path(&vec![], 1).unwrap();
+        n.add_cargo_after_path(&vec![0], 2).unwrap();
+        assert_eq!(PathResult::RequestedPathNotAvailable, n.get_next_path(&vec![1]));
+    }
+
+    #[test]
+    #[should_panic]
+    fn node_get_next_path_from_last_unwrap() {
+        let mut n = Node::new(0u8);
+        n.add_cargo_under_path(&vec![], 1).unwrap();
+        n.add_cargo_after_path(&vec![0], 2).unwrap();
+        let path_result = n.get_next_path(&vec![1]);
+        path_result.unwrap();
+    }
+
+    #[test]
+    fn node_get_next_path_next_sibling() {
+        let mut n = Node::new(0u8);
+        n.add_cargo_under_path(&vec![], 1).unwrap();
+        n.add_cargo_after_path(&vec![0], 2).unwrap();
+
+        let path_result = n.get_next_path(&vec![0]);
+        assert!(path_result.is_found());
+        let next_path = path_result.unwrap();
+        assert_eq!(vec![1], next_path);
+        assert_eq!(&2, n.borrow_cargo_by_path(&next_path).unwrap());
+    }
+
+    #[test]
+    fn node_get_next_path_root_to_child() {
+        let mut n = Node::new(0u8);
+        n.add_cargo_under_path(&vec![], 1).unwrap();
+        n.add_cargo_after_path(&vec![0], 2).unwrap();
+        assert_eq!(PathResult::Found(vec![0]), n.get_next_path(&vec![]));
+    }
+
+    #[test]
+    fn node_get_next_path_non_root_to_child() {
+        let mut n = Node::new(0u8);
+        n.add_cargo_under_path(&vec![], 1).unwrap();
+        n.add_cargo_after_path(&vec![0], 2).unwrap();
+        n.add_cargo_under_path(&vec![1], 3).unwrap();
+        n.add_cargo_after_path(&vec![1], 4).unwrap();
+        assert_eq!(PathResult::Found(vec![1, 0]), n.get_next_path(&vec![1]));
+    }
+
+    #[test]
+    fn node_get_next_path_non_child_to_parents_sibling() {
+        let mut n = Node::new(0u8);
+        n.add_cargo_under_path(&vec![], 1).unwrap();
+        n.add_cargo_after_path(&vec![0], 2).unwrap();
+        n.add_cargo_under_path(&vec![1], 3).unwrap();
+        n.add_cargo_after_path(&vec![1], 4).unwrap();
+        assert_eq!(PathResult::Found(vec![2]), n.get_next_path(&vec![1, 0]));
+    }
+
+    #[test]
+    fn node_get_next_path_non_child_to_grandparents_sibling() {
+        let mut n = Node::new(0u8);
+        n.add_cargo_under_path(&vec![], 1).unwrap();
+        n.add_cargo_after_path(&vec![0], 2).unwrap();
+        n.add_cargo_under_path(&vec![1], 3).unwrap();
+        n.add_cargo_under_path(&vec![1, 0], 4).unwrap();
+        n.add_cargo_after_path(&vec![1], 5).unwrap();
+        assert_eq!(PathResult::Found(vec![2]), n.get_next_path(&vec![1, 0, 0]));
+    }
+
+    #[test]
+    fn node_get_next_path_from_last_is_grandchild() {
+        let mut n = Node::new(0u8);
+        n.add_cargo_under_path(&vec![], 1).unwrap();
+        n.add_cargo_after_path(&vec![0], 2).unwrap();
+        n.add_cargo_under_path(&vec![1], 3).unwrap();
+        n.add_cargo_under_path(&vec![1, 0], 4).unwrap();
+        assert_eq!(PathResult::RequestedPathNotAvailable, n.get_next_path(&vec![1, 0, 0]));
     }
 }
